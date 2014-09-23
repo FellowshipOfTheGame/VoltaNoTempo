667eb8e2bcea2b95ae8e29d79a2c6af3337b2be3
Guilherme S.G <kuramagui@gmail.com>
2014-09-23 12:02:39 +0200

    configurando projeto no XCode

--- editor/main.cpp
+++ editor/main.cpp
@@ -6,15 +6,25 @@
 *
 * -- Open Source, Open to learn, Open to distribute. 
 * Just don't misrepresent my credits and time spend to make this library. Thanks.
-*/
-#include "editor_mapa.h"
 
-int main() {
+#include "editor_mapa.h"*/
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[])
+{
+    /*
     int num = 0;
     if (!editor_mapa()) {
         fprintf(stderr, "Error on execution\n");
         num = 1;
     }
-    return num;
+    return num;*/
+    
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+        return -1;
+    
+    printf("Editor");
+    
+    return 0;
 }
 
