f0c43cc641c8d1992374efa2c96d0fe26a8a08d8
Guilherme S.G <kuramagui@gmail.com>
2014-09-30 15:42:59 +0200

    Ajustando o esqueleto para SDL2 ficar mais centralizada. Precisa de mais cleanup e mais testes

---
+++ src2/View/Engine.h
@@ -0,0 +1,38 @@
+//
+//  Engine.h
+//  VoltaOSX
+//
+//  Created by Guilherme S.G. on 9/30/14.
+
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+
+#include "GraphicsModule.h"
+#include "EventHandler.h"
+#include "TTFmodule.h"
+#include "AudioModule.h"
+
+class Engine
+{
+protected:
+    GraphicsModule *graphics;
+    EventHandler *event;
+    TTFmodule *ttf;
+    AudioModule *mixer;
+    
+public:
+    virtual ~Engine() {}
+    virtual bool init() = 0;
+    virtual void pollEvent(EventType *type_, EventCode *code_) = 0;
+    virtual bool shouldRender() = 0;
+    virtual void updateContext() = 0;
+    virtual void clear() = 0;
+    
+    virtual GraphicsModule* getGraphicsModule() {return graphics;}
+    virtual EventHandler* getEventHandler()     {return event;}
+    virtual TTFmodule* getTTFmodule()           {return ttf;}
+    virtual AudioModule* getAudioModule()       {return mixer;}
+    
+};
+
+#endif
